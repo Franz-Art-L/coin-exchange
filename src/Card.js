a65bab97fc277a7dc318dbde4e31291f92165d10
Franz-Art-L <francislandia@icloud.com>
2021-01-04 23:57:09 +0800

    styled compinents update

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+
+var { Component } = React;
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+                Card
+            </div>
+        )
+    }
+}
